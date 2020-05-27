bb7c052c0237ddaaa6696ef214173d8133821c2c
ramagg <rg1998@hotmail.es>
2020-05-27 15:28:59 -0300

    Dialog path working

--- AudioPlayerApp/index.js
+++ AudioPlayerApp/index.js
@@ -1,5 +1,8 @@
 const electron = require('electron')
+
 const app = electron.app
+const dialog = electron.dialog
+
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
@@ -9,12 +12,23 @@ const isDev = require('electron-is-dev')
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 500,
+    minHeight: 500,
+    minWidth: 400,
+    // resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`,
   )
+
+  // mainWindow.removeMenu()
   mainWindow.on('closed', () => (mainWindow = null))
 }
 
@@ -30,4 +44,22 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
+})
+
+// Interface
+const { ipcMain } = require('electron')
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'Electron response')
+
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ['openFile', 'openDirectory'],
+    })
+    .then((result) => {
+      event.reply('asynchronous-reply', [result.canceled, result.filePaths])
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
--- AudioPlayerApp/src/components/playlist/Actions.js
+++ AudioPlayerApp/src/components/playlist/Actions.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
+const { ipcRenderer } = window.require('electron')
+
+// Hooks
+const getData = () => {
+  return undefined
+}
+
+const reqData = () => {
+  ipcRenderer.send('asynchronous-message', 'Requesting the data')
+}
+
+// Component
 function Actions() {
+  useEffect(() => {
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+      console.log(arg)
+    })
+  }, [])
+
   return (
     <div className="actions">
       <input className="path" />
-      <button><i className="far fa-folder-open"></i></button>
+      <button onClick={() => reqData()}>
+        <i className="far fa-folder-open"></i>
+      </button>
     </div>
   )
 }
